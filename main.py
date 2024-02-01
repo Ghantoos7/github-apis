4206c58c34edc6411b3fe195c340137c119cfcfb
GHNATOS Georgio <gghnatos@murex.com>
2024-02-01 18:28:40 +0200

    Config <> Fixed vercel json configuration

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
 from flask import Flask, jsonify
 from github import Github, GithubException
 from github import Auth
-from dotenv import load_dotenv
-import os
 
-load_dotenv()
 
 AUTH_KEY = os.getenv('AUTH_KEY')
 
