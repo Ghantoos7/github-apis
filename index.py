d5d80efcf198540abe1a9f75d308eb1dfb35ad4b
Ghantoos7 <ghnatios1@gmail.com>
2024-03-02 17:06:27 +0200

    Main <> Fixed APIs

--- index.py
+++ index.py
@@ -4,7 +4,9 @@ from github import Github, GithubException
 from github import Auth
 import os
 
-AUTH_KEY = os.getenv('AUTH_KEY')
+
+AUTH_KEY = os.getenv("AUTH_KEY")
+
 
 app = Flask(__name__)
 
@@ -12,7 +14,6 @@ app.config['CACHE_TYPE'] = 'simple'
 cache = Cache(app)
 
 @app.route('/github/user/commits/total')
-@cache.cached(timeout=3600)
 def get_total_own_commits():
 
     auth = Auth.Token(AUTH_KEY)
@@ -27,14 +28,36 @@ def get_total_own_commits():
                 commits = repo.get_commits(author=user.login)
                 total_own_commits += commits.totalCount
             except GithubException as e:
-                
-                if e.status == 409:
+                if e.status == 409: 
                     continue  
                 else:
                     raise  
     except GithubException as e:
         return jsonify({"error": str(e), "message": "Error fetching data from GitHub"}), 500
 
-    return jsonify({ "schemaVersion": 1, "label": "Total Commits", "message": str(total_own_commits), "color": "red" })
+    return jsonify({"schemaVersion": 1, "label": "Total Commits", "message": str(total_own_commits), "color": "red"})
+
+
+@app.route('/github/user/problems/total')
+def get_total_own_problems():
+    
+        auth = Auth.Token(AUTH_KEY)
+        g = Github(auth=auth)
+    
+        total_own_problems = 0
+
+        try:
+            user = g.get_user()
+            repo = user.get_repo("LeetCode-Problems")
+            contents = repo.get_contents("")
+            for content_file in contents:
+                if content_file.type == "dir":
+                    total_own_problems += 1
+        except GithubException as e:
+            return jsonify({"error": str(e), "message": "Error fetching data from GitHub", "Auth key is set: ": AUTH_KEY is not None}), 500
+        
+        return jsonify({ "schemaVersion": 1, "label": "Total Problems Solved", "message": str(total_own_problems), "color": "blue" })
+           
+    
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
