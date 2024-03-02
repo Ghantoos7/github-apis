b6b022db274d59d99940802a0efb0476a245108e
Ghantoos7 <ghnatios1@gmail.com>
2024-03-02 16:43:00 +0200

    Main <> Debugging

--- index.py
+++ index.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify
 from flask_caching import Cache
 from github import Github, GithubException
+from github import Auth
 import os
 
 AUTH_KEY = os.getenv('AUTH_KEY')
@@ -11,10 +12,10 @@ app.config['CACHE_TYPE'] = 'simple'
 cache = Cache(app)
 
 @app.route('/github/user/commits/total')
-@cache.cached(timeout=3600)
 def get_total_own_commits():
 
-    g = Github(AUTH_KEY)
+    auth = Auth.Token(AUTH_KEY)
+    g = Github(auth=auth)
 
     total_own_commits = 0
 
@@ -25,6 +26,7 @@ def get_total_own_commits():
                 commits = repo.get_commits(author=user.login)
                 total_own_commits += commits.totalCount
             except GithubException as e:
+                
                 if e.status == 409:
                     continue  
                 else:
@@ -33,3 +35,29 @@ def get_total_own_commits():
         return jsonify({"error": str(e), "message": "Error fetching data from GitHub"}), 500
 
     return jsonify({ "schemaVersion": 1, "label": "Total Commits", "message": str(total_own_commits), "color": "red" })
+
+
+
+@app.route('/github/user/problems/total')
+def get_total_own_problems():
+    
+        auth = Auth.Token(AUTH_KEY)
+        g = Github(auth=auth)
+    
+        total_own_problems = 0
+
+        try:
+            user = g.get_user()
+            repo = user.get_repo("LeetCode-Problems")
+            contents = repo.get_contents("")
+            for content_file in contents:
+                if content_file.type == "dir":
+                    total_own_problems += 1
+        except GithubException as e:
+            return jsonify({"error": str(e), "message": "Error fetching data from GitHub", "Auth key is set: ": AUTH_KEY is not None}), 500
+        
+        return jsonify({ "schemaVersion": 1, "label": "Total Problems Solved", "message": str(total_own_problems), "color": "blue" })
+           
+    
+if __name__ == '__main__':
+    app.run()
