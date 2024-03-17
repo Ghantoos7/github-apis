585d29a5a9dd8e20234e409ccef6101a26718c98
Ghantoos7 <ghnatios1@gmail.com>
2024-03-17 13:23:15 +0200

    Main <> Added paralleization for faster performance

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify
 from flask_caching import Cache
 from github import Github, GithubException
 import os
+from concurrent.futures import ThreadPoolExecutor, as_completed
 
 AUTH_KEY = os.getenv('AUTH_KEY')
 
@@ -11,25 +12,34 @@ app.config['CACHE_TYPE'] = 'simple'
 app.config['CACHE_DEFAULT_TIMEOUT'] = 43200
 cache = Cache(app)
 
+def fetch_commit_count(repo, user_login):
+    try:
+        commits = repo.get_commits(author=user_login)
+        return commits.totalCount
+    except GithubException as e:
+        if e.status == 409:  
+            return 0
+        else:
+            raise
+
+
 @app.route('/github/user/commits/total')
-@cache.cached(timeout=43200, key_prefix='total_own_commits')
 def get_total_own_commits():
     g = Github(login_or_token=AUTH_KEY)
     total_own_commits = 0
     try:
         user = g.get_user()
-        for repo in user.get_repos():
-            try:
-                commits = repo.get_commits(author=user.login)
-                total_own_commits += commits.totalCount
-            except GithubException as e:
-                if e.status == 409:
-                    continue
-                else:
-                    raise
+        with ThreadPoolExecutor(max_workers=10) as executor:
+            future_to_repo = {executor.submit(fetch_commit_count, repo, user.login): repo for repo in user.get_repos()}
+            for future in as_completed(future_to_repo):
+                try:
+                    total_own_commits += future.result()
+                except GithubException as e:
+                    if e.status != 409:
+                        return jsonify({"error": str(e), "message": "Error fetching commit count"}), 500
     except GithubException as e:
         return jsonify({"error": str(e), "message": "Error fetching data from GitHub"}), 500
-    return jsonify({ "schemaVersion": 1, "label": "Total Commits", "message": str(total_own_commits), "color": "red" })
+    return jsonify({"schemaVersion": 1, "label": "Total Commits", "message": str(total_own_commits), "color": "red"})
 
 @app.route('/github/user/repos/<repo_name>/folders')
 def get_repo_folder_count(repo_name):
